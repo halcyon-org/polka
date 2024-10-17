22b73be976991aac4174a6797a2b3a9faf800f88
masatarounakakita <masatarounakakita@gmail.com>
2024-10-17 13:41:50 +0900

    style: indent fixed

--- app/component/Header/Header.stories.ts
+++ app/component/Header/Header.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { createRemixStub } from "@remix-run/testing"
+import { createRemixStub } from "@remix-run/testing";
 
 import { Header } from "./Header";
 
