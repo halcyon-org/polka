2a273e2b47a18f7a7027818dd60a6db1f35bc786
Itsuki Shirahige <shiron4710.dev@gmail.com>
2024-10-17 23:34:50 +0900

    feat: Impl theme color (#64)
    --- commit logs ---
    * feat: Impl theme color

--- app/styles/theme.css.ts
+++ app/styles/theme.css.ts
@@ -2,20 +2,34 @@ import { createGlobalTheme, createThemeContract } from "@vanilla-extract/css";
 
 export const vars = createThemeContract({
   color: {
+<<<<<<< HEAD
     background: "#D9D9D9",
     text: "3C4063",
   },
   typography: {
     fontFamily: {
       roboto: "Roboto, sans-serif",
+=======
+    background: "",
+    text: "",
+  },
+  typography: {
+    fontFamily: {
+      roboto: "",
+>>>>>>> 0d505cb (feat: Impl theme color (#64))
     },
   },
 });
 
 createGlobalTheme(":root", vars, {
   color: {
+<<<<<<< HEAD
     text: "#3C4063",
     background: "#D9D9D9",
+=======
+    background: "#D9D9D9",
+    text: "#3C4063",
+>>>>>>> 0d505cb (feat: Impl theme color (#64))
   },
   typography: {
     fontFamily: {
