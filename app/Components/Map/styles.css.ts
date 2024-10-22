99182f7e95522dabc08b221e3613a85161cb3f99
r03i22 <103725932+r03i22@users.noreply.github.com>
2024-10-22 19:04:02 +0900

    style: containerStyle -> container

--- app/Components/Map/styles.css.ts
+++ app/Components/Map/styles.css.ts
@@ -1,9 +1,9 @@
-export const containerStyle ={
+export const container = {
   width: "108rem",
   height: "50.7rem",
 };
 
 export const center = {
-  lat: 34.8503, 
-  lng: 136.582, 
+  lat: 34.8503,
+  lng: 136.582,
 };
