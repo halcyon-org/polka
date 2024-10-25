ff663def2c02eefe0bbd49e5a6775c5a341c0246
shiron <shiron4710.dev@gmail.com>
2024-10-25 11:44:50 +0900

    fix: rm storybook env

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,15 +13,6 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
-  viteFinal: async (config) => {
-    config.define = {
-      ...config.define,
-      "process.env.GOOGLE_MAPS_API_KEY": JSON.stringify(
-        process.env.GOOGLE_MAPS_API_KEY,
-      ),
-    };
-    return config;
-  },
 };
 
 export default config;
