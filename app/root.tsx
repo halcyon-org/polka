cf3a2b31b3dcdfb8c360078b65e189a96cbb7fb9
Souma <102800255+6ouma@users.noreply.github.com>
2024-10-27 22:25:03 +0900

    refactor(root): Use the Remix MetaFunction for every cases
    Don't write meta tags directly in the JSX.

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,8 @@ export const links: LinksFunction = () => [
 ];
 
 export const meta: MetaFunction = () => [
+  { charSet: "utf-8" },
+  { name: "viewport", content: "width=device-width, initial-scale=1" },
   { name: "robots", content: "noindex" },
 ];
 
@@ -30,8 +32,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
